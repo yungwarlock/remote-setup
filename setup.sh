565d174c2f3fad5ab3af088b29c3b669d6572236
Damian <damiakpan@gmail.com>
2023-12-26 11:52:05 +0000

    Fixed issues

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-pushd .
-  cd /workspace
+pushd /workspaces
   wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+
+  chmod u+x ./nvim.appimage
   ./nvim.appimage --appimage-extract
 
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  cp $OLDPWD/nvim.tar.gz .
 
   tar -xzvf ./nvim.tar.gz
   cp -r nvim ~/.config/
@@ -14,5 +17,7 @@ pushd .
   ./squashfs-root/usr/bin/nvim +PlugInstall +qall
 popd
 
-/workspace/squashfs-root/usr/bin/nvim
+echo "alias nvim=/workspaces/squashfs-root/usr/bin/nvim" >> ~/.bashrc
+
+/workspaces/squashfs-root/usr/bin/nvim
 
